695494e797e82455a4ecee6554dc7a6da813f28a
Robert Marwil <robertmarwil@Roberts-MacBook-Air.local>
2015-02-19 11:55:31 -0700

    all methods work

--- split_join_warmup.rb
+++ split_join_warmup.rb
@@ -10,31 +10,39 @@ world_cup_news
 
 # enter your solutions inside the methods
 def goal(goal_message)
-
+  goal_message*2
 end
 
 def generate_an_array_of_teams(team_list)
-
+  team_list.split(", ").to_a
 end
 
 def number_of_teams(team_list)
-
+  team_list.split(", ").to_a.length
 end
 
 def return_australia(team_list)
-
+  team_list.split(", ").find {|x| x == 'Australia'}
 end
 
 def starts_with_C(team_list)
-
+  team_list.split(", ").select {|x| x[0] == "C"}
 end
 
 def block_string_to_single_line(long_string)
-
+  long_string.tr("\n"," ").split(", ").join(" ").strip
 end
 
 def capitalize_every_third_word(long_string)
-
+  a = long_string.tr("\n"," ").downcase.split(", ").to_a
+  a.map.with_index do |x, i|
+    if i == 0 || i%3 == 0
+      a[i] = x.capitalize
+    else
+      a[i] = x
+    end
+  end
+  a.join(" ").strip
 end
 
 check("goal method", goal(goal_message) == "GOOOOOOOAL!!!!GOOOOOOOAL!!!!")
